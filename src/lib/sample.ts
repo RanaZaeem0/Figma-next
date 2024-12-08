8e838a14af7ae643881e03384b78b0209ec13f57
Rana Zaeem <ranazaeem34@gmail.com>
2024-12-08 21:09:02 +0500

    asdasdas

--- src/lib/sample.ts
+++ src/lib/sample.ts
@@ -44,3 +44,65 @@ export const testimonailData = [
     },
 
 ]
+
+export const productsData = [
+    {
+      productImg: "/Frame 32.png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 1,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+    {
+      productImg: "/Frame 38.png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 2,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+    {
+      productImg: "/Frame 38 (1).png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 3,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+    {
+      productImg: "/Frame 32 (1).png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 4,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+    {
+      productImg: "/Frame 33.png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 5,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+    {
+      productImg: "/Frame 33 (1).png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 6,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+    {
+      productImg: "/Frame 34 (1).png",
+      name: "T-Shirt best",
+      star: 4,
+      id: 7,
+      price: "140$",
+      category: ["SHIRT", "NEW", "SALES"],
+    },
+  ];
+  
+  // Log the array to verify
+  
